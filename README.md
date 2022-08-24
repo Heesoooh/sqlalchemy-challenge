afaac7017acfbb983afd4ff4f1a41479f4a56448
Heesoooh <heesoo0216@gmail.com>
2022-08-23 22:03:10 -0400

    updating file

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # sqlalchemy-challenge
+
+## Purpose of Project
+In order to help with trip planning to Honolulu, Hawaii, I need to do some climate analysis on the area.
+In this assignment, you'll complete challenge using Python, SQLAlchemy, Pandas and Matplotlib. 
+### Part 1: Climate Analysis and Exploration
+In this section, you’ll use Python and SQLAlchemy to perform basic climate analysis and data exploration of your climate database. Complete the following tasks by using SQLAlchemy ORM queries, Pandas, and Matplotlib.
+- Use the provided starter notebook and hawaii.sqlite files to complete your climate analysis and data exploration.
+- Use SQLAlchemy’s create_engine to connect to your SQLite database.
+- Use SQLAlchemy’s automap_base() to reflect your tables into classes and save a reference to those classes called Station and Measurement.
+
+Link Python to the database by creating a SQLAlchemy session.
+Important: Don't forget to close out your session at the end of your notebook.
+
+Precipitation Analysis:
+To perform an analysis of precipitation in the area, do the following:
+- Find the most recent date in the dataset.
+- Using this date, retrieve the previous 12 months of precipitation data by querying the 12 previous months of data. Note: Do not pass in the date as a variable to your query.
+- Select only the date and prcp values.
+- Load the query results into a Pandas DataFrame, and set the index to the date column.
+- Sort the DataFrame values by date.
+
+Station Analysis:
+- To perform an analysis of stations in the area, do the following:
+- Design a query to calculate the total number of stations in the dataset.
+- Design a query to find the most active stations (the stations with the most rows).
+- Design a query to retrieve the previous 12 months of temperature observation data (TOBS).
+
+### Part 2: Design Your Climate App
+Now that you have completed your initial analysis, you’ll design a Flask API based on the queries that you have just developed. 
+- Homepage
+- Precipitation
+- Stations
+- tobs
+- Start and End
+
+## Analysis 
+
+### Climate Analysis and Exploration
+In this challenge we created <a href="climate_starter.ipynb">code</a> to do precipitation and stations analysis 
+<img src="Images/precipitation.png" width="500">
+
+### Climate App
+In this challenge we created <a href="app.py">code</a> to design a flask API based on the data that I developed.
+
+## Results
+From analysis, in between Sep and October 2016 had most precipitation over 6inches followed by the May 2017 with just over 6 inches. From 2016-2017, maximum precipitation was 6.7 inches. 
+There are total 9 stations, and from the Waihee Station Temperature Observations, most frequent temperature was 75.
+
+## Recommandation 
+From the analysis, the best time to travel Honolulu, Hawaii is April, June, July and August. 
---
+++ app.py
@@ -0,0 +1,154 @@
+import numpy as np
+import sqlalchemy
+from flask import Flask, json, jsonify
+import datetime as dt
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from sqlalchemy import inspect
+
+# Database Setup
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+Base.classes.keys()
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Flask Setup
+app = Flask(__name__)
+
+# Flask routes
+@app.route("/")
+def welcome():
+    # List all available api routes
+    return (
+        f"<left>"
+        f"Available Routes:<br/><br/>"
+        f"Precipitation Data: <b><u>/api/v1.0/precipitation<br/></b></u>"
+        f"List of Stations: <b><u>/api/v1.0/stations<br/></b></u>"
+        f"Temperature Observations of 2016 - 2017: <b><u>/api/v1.0/tobs<br/></b></u>"
+        f"Temperature from Start Date (yyyy-mm-dd): <b><u>/api/v1.0/<start></b></u><br/>"
+        f"Temperature range Start - End Date (yyyy-mm-dd): <b><u>/api/v1.0/<start>/<end><b><u>"
+        f"</center>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create session (link) from Python to the DB
+    session = Session(engine)
+
+    # Convert the query results to a dictionary using `date` as the key and `prcp` as the value.
+    prcp_results = session.query(Measurement.date, Measurement.prcp).\
+                filter(Measurement.date > (dt.date(2017, 8, 23) - dt.timedelta(days=365))).all()
+
+    session.close()
+
+    # Create a dictionary from the query results
+    rain_data = []
+    for date, prcp in prcp_results:
+        prcp_dict = {}
+        prcp_dict['date'] = date
+        prcp_dict['prcp'] = prcp
+        rain_data.append(prcp_dict)
+    
+    return jsonify(rain_data)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Return a JSON list of stations from the dataset
+    results = session.query(Station.station, Station.name).all()
+    session.close()
+
+    all_stations = list(np.ravel(results))
+
+    return jsonify(all_stations)
+
+@app.route("/api/v1.0/tobs")
+def observations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query the dates and temperature observations of the most active station for the previous year of data.
+
+    stationCount = engine.execute('SELECT m.station, COUNT(m.station) FROM Measurement m GROUP BY m.station ORDER BY COUNT(m.station) DESC').fetchall()
+
+    activeStation = stationCount[0].station
+
+    results = session.query(Measurement.date, Measurement.tobs).\
+                    filter((Measurement.station == activeStation) &
+                           (Measurement.date > (dt.date(2017, 8, 18) - dt.timedelta(days= 365)))).all()
+
+    session.close()
+
+
+    # Return a JSON list of temperature observations (TOBS) for the previous year.
+    temp_list = list(np.ravel(results))
+
+    return jsonify(temp_list)
+
+# Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a given start or start-end range.
+# When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than or equal to the start date.
+
+@app.route("/api/v1.0/<start>")
+def start_date(start):
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query the min, max, avg temps
+    tobs_results = session.query(Measurement.date, func.avg(Measurement.tobs), func.min(Measurement.tobs), func.max(Measurement.tobs).filter(Measurement.date >= start)).all()
+
+    # Close session
+    session.close()
+
+    # Create a dictionary from the query results
+    temp_data = []
+    for date, tMin, tMax, tAVG in tobs_results:
+        tobs_dict = {}
+        # tobs_dict['date'] = date
+        tobs_dict['TMIN'] = tMin
+        tobs_dict['TMAX'] = tMax
+        tobs_dict['TAVG'] = tAVG
+        temp_data.append(tobs_dict)
+    
+    # Jsonify
+    return jsonify(temp_data)
+
+# When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates from the start date through the end date (inclusive).
+@app.route("/api/v1.0/<start>/<end>")
+def start_end_date(start, end):
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query the min, max, avg temps
+    tobs_results = session.query(Measurement.date, func.avg(Measurement.tobs), func.min(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+
+    # Close session
+    session.close()
+
+    # Create a dictionary from the query results
+    temp_data = []
+    for date, tMin, tMax, tAVG in tobs_results:
+        tobs_dict = {}
+        # tobs_dict['date'] = date
+        tobs_dict['TMIN'] = tMin
+        tobs_dict['TMAX'] = tMax
+        tobs_dict['TAVG'] = tAVG
+        temp_data.append(tobs_dict)
+    
+    # Jsonify
+    return jsonify(temp_data)
+
+# Launch app
+if __name__ == '__main__':
+    app.run(debug= True)
